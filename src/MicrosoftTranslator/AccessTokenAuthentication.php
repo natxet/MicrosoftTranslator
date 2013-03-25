d07a8a32959b67666b8f8c19330db82cfe1a98a5
natxet <icofre@gmail.com>
2013-03-25 13:41:46 +0100

    Avoid error on access if not $error defined

--- src/MicrosoftTranslator/AccessTokenAuthentication.php
+++ src/MicrosoftTranslator/AccessTokenAuthentication.php
@@ -49,7 +49,7 @@ class AccessTokenAuthentication
         curl_close( $ch );
         //Decode the returned JSON string.
         $objResponse = json_decode( $strResponse );
-        if ($objResponse->error) {
+        if (!empty( $objResponse->error )) {
             throw new \Exception( $objResponse->error_description );
         }
         return $objResponse->access_token;
